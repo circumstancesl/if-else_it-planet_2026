6594dbde50724dba961a10c2d6c086f72a2951e9
circumstancesl <gsrmakill@mail.ru>
2026-03-28 23:32:21 +0700

    fix

---
+++ backend/middleware/companyVerification.middleware.js
@@ -0,0 +1,19 @@
+const createError = require('http-errors');
+const { Companies } = require('../db/models');
+
+async function companyVerificationMiddleware(req, res, next) {
+  const company = await Companies.findOne({
+    where: { userId: req.user.id }
+  })
+
+  if (!company) {
+    throw createError(404, 'Компания не найдена');
+  }
+
+  if (company.verification_status !== 'accepted') {
+    throw createError(403, 'Только для верифицированных компаний');
+  }
+  next();
+}
+
+module.exports = companyVerificationMiddleware;
